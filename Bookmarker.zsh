cfcc022c08942d120ebf65b2ed76fa1fa58784aa
iMorph <iMorpheus@users.noreply.github.com>
2021-07-15 12:04:33 +0900

    Create Bookmarker.zsh

---
+++ Bookmarker.zsh
@@ -0,0 +1,146 @@
+#!/bin/zsh
+
+### BOOKMARKER: an iMorphTV Creation ##
+# Collect & stores bookmarks locally 
+# Bookmarker collects the following:
+# - Page Title
+# - Site Name
+# - URL Scheme
+# - URL Subdmain
+# - URL Second Level Domain
+# - URL Top Level Domain
+# - Path to whatever
+#######################################
+
+
+###     FUNCTIONS LIST & PURPOSE    ###
+# - PrintHeader:
+# 	* Clears Screen
+#	* Prints Banner
+# - GetTitle
+#	* Captures Page Title
+# - GetSiteName
+#	* Captures Site Name
+# - GetScheme
+#	* Captures Scheme
+# - GetSubdomain
+#	* Captures Subdmain
+# - GetSecondLevelDomain
+#	* Captures Second Level Domain
+# - GetTLD
+#	* Captures Top Level Domain
+# - GetPath
+#	* Captures path to resource?
+#####################################
+
+
+### FUNCTIONS ###
+
+Function PrintHeader (){
+clear
+Cols=$(tput cols)
+echo "Bookmarker ver. 0.1" | boxes -p a56 -s "$Cols"x1
+}
+
+Function GetTitle (){
+read -r "Title?Page Title: "
+PrintHeader
+}
+
+Function GetSiteName () {
+read -r "SiteName?Site Name: "
+PrintHeader
+}
+
+Function GetScheme () {
+PS3="Select Scheme: "
+select Scheme in http:// https:// ftp:// other none 
+do case $Scheme in
+other) read -r "Scheme?Enter Scheme: " ;;
+none) Scheme="";;
+esac
+break
+done
+
+PrintHeader
+}
+
+Function GetSubdomain () {
+PS3="Select Subdomain: "
+select Subdomain in www. blog. wiki. en. jp. other none
+do case $Subdomain in
+other) read -r "Subdomain?Enter Subdomain: " ;;
+none) Subdomain="";;
+esac
+break
+done
+
+PrintHeader
+}
+
+Function GetSecondLevelDomain () {
+read -r "SLD?Second Level Domain: "
+PrintHeader
+}
+
+Function GetTLD () {
+PS3="Select TLD: "
+select TLD in .com .org .edu .gov .net .xyz .cc .co.jp other none
+do
+case $Domain in
+other) 
+	read -r "TLD?Enter TLD: " 
+	TLD=".$TLD";;
+none) TLD="";;
+*)
+esac
+break
+done
+PrintHeader
+}
+
+Function GetPath () {
+read -r "Path?Path:/ "
+PrintHeader
+}
+
+#### MAIN FUNCTION ###
+Function GetBookMark(){
+PrintHeader
+GetTitle
+GetSiteName
+GetScheme
+GetSubdomain
+GetSecondLevelDomain
+GetTLD
+GetPath
+}
+
+### AVAILABLE FUNCTIONS ###
+# - PrintHeader
+# - GetTitle
+# - GetSiteName
+# - GetScheme
+# - GetSubdomain
+# - GetSecondLevelDomain
+# - GetTLD
+# - GetPath
+##########################
+
+
+### INPUT ###
+GetBookMark
+DateSaved=$(date +'%Y-%m-%d')
+
+### MANIPULATION ##
+Entry=$(print "$Title - $SiteName,$Scheme$Subdomain$SLD$TLD/$Path,$DateSaved")
+
+
+### OUTPUT ###
+echo $Entry | pbcopy
+pbpaste >> [Path to Bookmark.csv]
+
+#### VIEW ####
+csvlook [Path to Bookmark.csv]
+sleep 10
+clear
